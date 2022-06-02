c0ea81fed0a1dc817029f0587d3f1156a87a7815
trinagle1289 <denny95090@gmail.com>
2022-06-02 16:04:38 +0800

    [feat] Add Activities

--- app/src/main/java/tw/edu/pu/s1071554/mobile_game_development_final_assignment/ExpenseInfoActivity.kt
+++ app/src/main/java/tw/edu/pu/s1071554/mobile_game_development_final_assignment/ExpenseInfoActivity.kt
@@ -3,10 +3,10 @@ package tw.edu.pu.s1071554.mobile_game_development_final_assignment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-// 主介面
-class MainActivity : AppCompatActivity() {
+// 支出資訊界面
+class ExpenseInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_expense_create)
     }
 }
--- app/src/main/java/tw/edu/pu/s1071554/mobile_game_development_final_assignment/IncomeInfoActivity.kt
+++ app/src/main/java/tw/edu/pu/s1071554/mobile_game_development_final_assignment/IncomeInfoActivity.kt
@@ -3,10 +3,11 @@ package tw.edu.pu.s1071554.mobile_game_development_final_assignment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-// 主介面
-class MainActivity : AppCompatActivity() {
+// 收入資訊界面
+class IncomeInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_income_create)
+
     }
 }
