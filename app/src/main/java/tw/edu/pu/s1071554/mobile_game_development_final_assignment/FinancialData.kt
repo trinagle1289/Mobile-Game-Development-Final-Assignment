bba8842610300be60a3ac77130431ebc2e31fdde
trinagle1289 <denny95090@gmail.com>
2022-06-06 19:34:26 +0800

    [feat] 新增資料庫的使用
    新增了 fin_db 資料庫
    從 fin_db 資料庫中新增了資料表 income_model, expense_model

---
+++ app/src/main/java/tw/edu/pu/s1071554/mobile_game_development_final_assignment/FinancialData.kt
@@ -0,0 +1,18 @@
+package tw.edu.pu.s1071554.mobile_game_development_final_assignment
+
+// 財務資料
+class FinancialData {
+
+    var id: Int = -1            // 資料 ID
+    var time: String = "//"     // 時間
+    var amount: Int = 0         // 金額
+    var message: String = ""    // 描述訊息
+
+    constructor(_i: Int, _t: String, _a: Int, _m: String) {
+        id = _i
+        time = _t
+        amount = _a
+        message = _m
+    }
+
+}
---
+++ app/src/main/java/tw/edu/pu/s1071554/mobile_game_development_final_assignment/FinancialListAdapter.kt
@@ -0,0 +1,6 @@
+package tw.edu.pu.s1071554.mobile_game_development_final_assignment
+
+class FinancialListAdapter {
+
+
+}
