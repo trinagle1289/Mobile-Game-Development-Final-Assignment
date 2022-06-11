2a35418204af4fee8b1fd55ac8cb441cbfdd51cf
trinagle1289 <denny95090@gmail.com>
2022-06-11 21:11:58 +0800

    [feat] 修改 ExpenseListActivity.kt 內容
    ExpenseListActivity.kt 修改成 跟 IncomeListActivity.kt 類似的內容

--- app/src/main/java/tw/edu/pu/s1071554/mobile_game_development_final_assignment/ExpenseListActivity.kt
+++ app/src/main/java/tw/edu/pu/s1071554/mobile_game_development_final_assignment/ExpenseListActivity.kt
@@ -5,43 +5,45 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.TextView
+import tw.edu.pu.s1071554.mobile_game_development_final_assignment.database.FinancialData
+import java.text.SimpleDateFormat
+import java.util.*
 
 // 支出列表介面
 class ExpenseListActivity : AppCompatActivity() {
 
-    // UI 變數
-    lateinit var tvTitle: TextView // 標題名稱
-    lateinit var btAdd: ImageButton // 新增名單
-    lateinit var btBack: ImageButton // 返回主頁面
+    lateinit var fdata: List<FinancialData>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_expense_list)
-
-        // 初始化
-        tvTitle = findViewById(R.id.expanse_list_title_text)
-        btAdd = findViewById(R.id.expanse_list_add_item_btn)
-        btBack = findViewById(R.id.expanse_list_back_btn)
         setView()
-        setButton()
-
     }
 
     // 設定畫面資料
     private fun setView() {
+        // 初始化UI變數
+        // 標題名稱
+        val currentDate: String = SimpleDateFormat("dd", Locale.getDefault()).format(Date())
+        val tvTitle: TextView = findViewById(R.id.expanse_list_title_text)
+        val title = currentDate + "月 收入表"
+        tvTitle.text = title
 
-    }
-
-    // 設定按鈕功能
-    private fun setButton() {
+        // 新增名單
+        val btAdd: ImageButton = findViewById(R.id.expense_create_add_btn)
         btAdd.setOnClickListener {
-            var it: Intent = Intent(this, ExpenseAddActivity::class.java)
+            val it = Intent(this, ExpenseAddActivity::class.java)
             startActivity(it)
         }
 
+        // 返回主頁面
+        val btBack: ImageButton = findViewById(R.id.expanse_list_back_btn)
         btBack.setOnClickListener {
             finish()
         }
+
+        // 載入資料
     }
+
 
 }
