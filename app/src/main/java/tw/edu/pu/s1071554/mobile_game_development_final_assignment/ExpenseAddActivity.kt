fdea083112f182594731e0563792f5193be265c9
trinagle1289 <denny95090@gmail.com>
2022-06-11 21:35:34 +0800

    [feat] 修改 ExpenseAddActivity.kt 內容
    ExpenseAddActivity.kt 修改成 跟 IncomeAddActivity.kt 類似的內容

--- app/src/main/java/tw/edu/pu/s1071554/mobile_game_development_final_assignment/ExpenseAddActivity.kt
+++ app/src/main/java/tw/edu/pu/s1071554/mobile_game_development_final_assignment/ExpenseAddActivity.kt
@@ -1,53 +1,67 @@
 package tw.edu.pu.s1071554.mobile_game_development_final_assignment
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.EditText
 import android.widget.ImageButton
+import java.text.SimpleDateFormat
+import java.util.*
 
 // 新增支出界面
 class ExpenseAddActivity : AppCompatActivity() {
 
     // UI 變數
-    lateinit var etTime: EditText // 時間文字
-    lateinit var etExpense: EditText // 輸入支出金額
-    lateinit var etDescribe: EditText // 輸入描述文字
-
-    lateinit var btAdd: ImageButton // 新增按鈕
-    lateinit var btBack: ImageButton // 返回按鈕
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_expense_add)
-
-        // 初始化
-        etTime = findViewById(R.id.expense_create_time_data)
-        etExpense = findViewById(R.id.expense_create_expense_data)
-        etDescribe = findViewById(R.id.expense_create_describe_data)
-        btAdd = findViewById(R.id.expense_create_add_btn)
-        btBack = findViewById(R.id.expense_create_back_btn)
         setView()
-        setButton()
-
     }
 
-    // 設定畫面資料
+    // 初始化UI變數
     private fun setView() {
-        etTime.text.clear()
-        etExpense.text.clear()
-        etDescribe.text.clear()
-    }
+        // 時間文字
+        val etTime: EditText = findViewById(R.id.expense_create_time_data)
+        val currentDate: String = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault()).format(Date())
+        etTime.setText(currentDate)
 
-    // 設定按鈕功能
-    private fun setButton() {
-        btAdd.setOnClickListener {
-            finish()
-        }
+        // 輸入收入金額
+        val etExpense: EditText = findViewById(R.id.expense_create_expense_data)
 
+        // 輸入描述文字
+        val etDescribe: EditText = findViewById(R.id.expense_create_describe_data)
+
+        // 返回按鈕
+        val btBack: ImageButton = findViewById(R.id.expense_create_back_btn)
         btBack.setOnClickListener {
             finish()
         }
+
+        // 新增按鈕
+        val replyIntent = Intent()
+        val btAdd: ImageButton = findViewById(R.id.expense_create_add_btn)
+        btAdd.setOnClickListener {
+            if(TextUtils.isEmpty(etExpense.text)) {
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                val time = etTime.text.toString()
+                val amount = etExpense.text.toString()
+                val description = etDescribe.text.toString()
+                replyIntent.putExtra(EXTRA_TIME, time)
+                replyIntent.putExtra(EXTRA_AMOUNT, amount)
+                replyIntent.putExtra(EXTRA_DESCRIPTION, description)
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()
+        }
+    }
+
+    companion object {
+        const val EXTRA_TIME = "tw.edu.pu.s1071554.mobile_game_development_final_assignment.TIME";
+        const val EXTRA_AMOUNT = "tw.edu.pu.s1071554.mobile_game_development_final_assignment.AMOUNT";
+        const val EXTRA_DESCRIPTION = "tw.edu.pu.s1071554.mobile_game_development_final_assignment.DESCRIPTION";
     }
 
 }
